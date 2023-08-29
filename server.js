b78b7e485b4ac087b1c8b7ef69827c0876d6e4f3
eduardo <eduardobnhr@gmail.com>
2023-08-29 11:21:52 -0400

    usando ForEach() e finalizando

--- server.js
+++ server.js
@@ -5,7 +5,38 @@ app.set("view engine", "ejs");
 
 //ta criando uma rota
 app.get("/", function(req, res){
-    res.render("pages/index");
+    const items = [
+        {
+            title: "D",
+            message: "esenvolver aplicações/serviços de forma facil"
+        },
+        {
+            title: "E",
+            message: "JS usa JavaScript para renderizar HTML"
+        },
+        {
+            title: "M",
+            message: "uito facil codar em Node"
+        },
+        {
+            title: "A",
+            message: " inicialização do node é mais rapida que a do Java"
+        },
+        {
+            title: "I",
+            message: "nstanciar um objeto é diferente de java"
+        },
+        {
+            title: "S",
+            message: "uper importante aprender node antes do react"
+        }
+    ];
+
+    const subtitle = "Uma linguagem de modelagem para criação de pagina HTML utilizando JS"
+    res.render("pages/index", {
+        qualitys: items,
+        subtitle: subtitle,
+    });
 })
 app.get("/sobre", function(req, res){
     res.render("pages/about");
