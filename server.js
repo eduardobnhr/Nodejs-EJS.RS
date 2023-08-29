0258c4c53aa07816516f2b744dd6fa79ba53274a
eduardo <eduardobnhr@gmail.com>
2023-08-29 10:06:08 -0400

    nova pagina e menu

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res){
     res.render("index");
 })
-app.get("sobre", function(req, res){
+app.get("/sobre", function(req, res){
     res.render("about");
 })
 
